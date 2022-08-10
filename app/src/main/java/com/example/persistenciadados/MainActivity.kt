18750af58cd14029f064bf3e86d02e6fcd939c09
Bruno Henrique <brunohosilva97@gmail.com>
2022-08-09 22:23:03 -0300

    :sparkles: Criado exemplo de persistencia de dados com arquivos

--- app/src/main/java/com/example/persistenciadados/MainActivity.kt
+++ app/src/main/java/com/example/persistenciadados/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileNotFoundException
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,25 +17,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // No click do botao salvar, ira iniciar a criacao do storage com a key saudacao
-        // e ira seta 2 constantes, nome e tratamento
-        // pegando os valores digitados pelo input e pelo spinner e associando
-        // os valores as chaves do storage
+        // Na acao do click em salvar
+        // criado uma variavel data onde ira concatenar
+        // o nome digitado com 2 pontos mais o item selecionado do spinner
+        // e passa esse data para dentro da funcao chamada gravaDadoArquivo
+        // e é renderizado um toast com a mensagem de salvo com sucesso
         btn_save.setOnClickListener(View.OnClickListener{
-            val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
-            val editor = saudacaoPersistencia.edit()
-
-            editor.putString("nome", input_name.text.toString())
-            editor.putString("tratamento", input_spinner.selectedItem.toString())
-            editor.apply()
+            val data = input_name.text.toString() +  ":" + input_spinner.selectedItem.toString()
+            gravaDadoArquivo("saudacao", data)
+            Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_LONG ).show()
         })
 
-
-        // Clicando no botao de exibir saudacao
-        // é navegado para uma outra activity
+        // Açao de click redireciona para a segunda activity
         btn_show.setOnClickListener(View.OnClickListener{
             val intent  = Intent(this, SaudacaoActivity::class.java)
             startActivity(intent)
         })
+    }
+
+
+    // Funcao onde é gerado um arquivo
+    // recebe 2 parametros, o nome do arquivo e o dado a ser salvo nela
+    // transforma o dado em um array de bytes
+    // realiza uma validacao em casos de erros
+    // sendo disparado no log algumas mensagens apontando o erro
+    private fun gravaDadoArquivo(fileName: String, data: String){
+        try {
+            val fs = openFileOutput(fileName, Context.MODE_PRIVATE)
+            fs.write(data.toByteArray())
+            fs.close()
+        }
+        catch (e: FileNotFoundException){ Log.i("gravaDadoArquivo", "FileNotFoundException")}
+        catch (e: IOException){ Log.i("gravaDadoArquivo", "IOException")}
     }
 }
--- app/src/main/java/com/example/persistenciadados/SaudacaoActivity.kt
+++ app/src/main/java/com/example/persistenciadados/SaudacaoActivity.kt
@@ -1,29 +1,54 @@
 package com.example.persistenciadados
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_saudacao.*
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.*
 
 class SaudacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saudacao)
 
-        // resgata o storage com a key saudacao
-        val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
-        // pega o valor do storage com a chave nome
-        val nome = saudacaoPersistencia.getString("nome", "")
-        // pega o valor do storage com a chave tratamento
-        val tratamento = saudacaoPersistencia.getString("tratamento", "")
+        // declarado na variavel tarefa a chamada da funcao recuperaDadoArquivo passando o nome do arquivo
+        val data = recuperaDadoArquivo("saudacao")
+        // realiza um split do string pelo termo ":"
+        val tokenizer = StringTokenizer(data, ":")
+        // verifica se no momento do split tem o campo nome, senao seta uma string com "Sem nome"
+        val nome = if(tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem nome"
+        // verifica se no momento do split o campo tratamento , senao seta uma string com "Sem tratamento"
+        val tratamento = if(tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem Tratamento"
 
-        // verificacao se vier o texto Sem tratamento do storage
-        // ira apresenta na tela somente o nome digitado
-        // senao ira concatenar o tratamento selecionado pelo spinner + o nome digitado
-        if(tratamento.equals("Sem tratamento")){
+        // realiza verificacao, se em tratamento esta vindo a string "Sem tratamento"
+        // caso verdadeiro no text da view ira renderizar somente o nome digitado e salve
+        // senao no text da view ira renderizar o tratamento mais o nome
+        if(tratamento.equals("Sem tratamento")) {
             text_saudacao.text = nome
         } else {
-            text_saudacao.text = tratamento + " " + nome
+            text_saudacao.text = "$tratamento $nome"
+        }
+
+    }
+
+
+    // funcao privada somente a essa classe
+    // recebendo 1 paramentro o nome do arquivo
+    // realiza a tentativa da abertura do arquivo
+    // realiza a leitura dos bytes do arquivo
+    private fun recuperaDadoArquivo(fileName: String): String {
+        return try {
+            val fi = openFileInput(fileName)
+            val data = fi.readBytes()
+            fi.close()
+            data.toString()
+            data.toString(Charset.defaultCharset())
+        } catch (e: FileNotFoundException) {
+            ""
+        } catch (e: IOException){
+            ""
         }
     }
 }
